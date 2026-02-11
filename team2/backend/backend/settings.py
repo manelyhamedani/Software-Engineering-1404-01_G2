886f4ee6fdc81107421b2d5102239c5ee5198183
Armin <armin.hamedazimi@cafebazaar.ir>
2026-02-11 12:24:36 +0330

    Restructure wiki project layout

--- team2/backend/backend/settings.py
+++ team2/backend/backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xeqby*=pc-#m2&v)^o+n&vzp$8rscz(ib7f4cx40=8(kanxjm-'
+SECRET_KEY = 'django-insecure-c@5(t1)qpic+=a_e8hpiimqs+24f93-2)a=(2fj&evqko669!q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
